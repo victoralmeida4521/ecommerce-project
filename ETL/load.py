f7d09be548f2ad114b5544cf1251602b7fe01a21
victoralmeida4521 <almeid4521@gmail.com>
2026-04-29 19:02:06 -0300

    refactor(load): adiciona fechamento automático da conexão com context manager

--- ETL/load.py
+++ ETL/load.py
@@ -3,101 +3,98 @@ print(pd.__version__)
 import sqlite3
 import os
 
-# -------------------------
-# Caminho do banco
-# ------------------------- 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-DB_path = os.path.join(BASE_DIR, "ecommerce.db")
+from config import DB_path
 
 
 def transform_data_silver_to_gold():
-    conn = sqlite3.connect(DB_path)
+    with sqlite3.connect(DB_path) as conn:
 
-    # -------------------------
-    # GOLD 1: KPIs
-    # -------------------------
-    orders = pd.read_sql_query(
-        "SELECT * FROM olist_orders_dataset_silver", conn
-    )
+        # -------------------------
+        # GOLD 1: KPIs
+        # -------------------------
+        orders = pd.read_sql_query(
+            "SELECT * FROM olist_orders_dataset_silver", conn
+        )
 
-    orders['order_purchase_timestamp'] = pd.to_datetime(
-        orders['order_purchase_timestamp']
-    )
+        orders['order_purchase_timestamp'] = pd.to_datetime(
+            orders['order_purchase_timestamp']
+        )
 
-    orders['date'] = orders['order_purchase_timestamp'].dt.date
+        orders['date'] = orders['order_purchase_timestamp'].dt.date
 
-    payments = pd.read_sql_query(
-        "SELECT * FROM olist_order_payments_dataset_silver", conn
+        payments = pd.read_sql_query(
+            "SELECT * FROM olist_order_payments_dataset_silver", conn
+            
+        )
+
+        orders_per_day = (
+            orders.groupby("date")["order_id"]
+            .nunique()
+            .reset_index(name="total_orders")
+        )
+
+
+
+        df = orders.merge(payments, on="order_id", how="left")
+
+        revenue_per_day = (
+            df.groupby("date")["payment_value"]
+            .sum()
+            .reset_index(name="total_revenue")
+        )
+
+        gold_daily = orders_per_day.merge(revenue_per_day, on= 'date')
+        gold_daily["avg_ticket"] = (
+            gold_daily["total_revenue"] / gold_daily["total_orders"]
+        )
+
+        gold_daily.to_sql(
+            "gold_daily_kpis",
+            conn,
+            if_exists="replace" , 
+            index = False
+        )
+
+        # -------------------------
+        # GOLD 2: Pedidos por cidade
+        # -------------------------
+        customers = pd.read_sql_query(
+            "SELECT * FROM olist_customers_dataset_silver", conn
+        )
+
+        df = orders.merge(customers, on="customer_id", how="left")
+
+        orders_by_city = (
+            df.groupby("customer_city")
+            .size()
+            .reset_index(name="total_orders")
+        )
+
+        orders_by_city.to_sql(
+            "gold_orders_by_city",
+            conn,
+            if_exists="replace",
+            index=False
+        )
+
+        # -------------------------
+        # GOLD 3: Produtos mais vendidos
+        # -------------------------
+        items = pd.read_sql_query(
+            "SELECT * FROM olist_order_items_dataset_silver", conn       
+        )
+
+        products_sold = (
+            items.groupby("product_id")
+            .size()
+            .reset_index(name="total_sold")
+            .sort_values(by="total_sold", ascending=False)
+        )
+
+        products_sold.to_sql("gold_top_products", conn, if_exists="replace", index=False)
         
-    )
-
-    orders_per_day = (
-        orders.groupby("date")["order_id"]
-        .nunique()
-        .reset_index(name="total_orders")
-    )
-
-
-
-    df = orders.merge(payments, on="order_id", how="left")
-
-    revenue_per_day = (
-        df.groupby("date")["payment_value"]
-        .sum()
-        .reset_index(name="total_revenue")
-    )
-
-    gold_daily = orders_per_day.merge(revenue_per_day, on= 'date')
-    gold_daily["avg_ticket"] = (
-        gold_daily["total_revenue"] / gold_daily["total_orders"]
-    )
-
-    gold_daily.to_sql(
-        "gold_daily_kpis",
-        conn,
-        if_exists="replace" , 
-        index = False
-    )
-
-    # -------------------------
-    # GOLD 2: Pedidos por cidade
-    # -------------------------
-    customers = pd.read_sql_query(
-        "SELECT * FROM olist_customers_dataset_silver", conn
-    )
-
-    df = orders.merge(customers, on="customer_id", how="left")
-
-    orders_by_city = (
-        df.groupby("customer_city")
-        .size()
-        .reset_index(name="total_orders")
-    )
-
-    orders_by_city.to_sql(
-        "gold_orders_by_city",
-        conn,
-        if_exists="replace",
-        index=False
-    )
-
-    # -------------------------
-    # GOLD 3: Produtos mais vendidos
-    # -------------------------
-    items = pd.read_sql_query(
-        "SELECT * FROM olist_order_items_dataset_silver", conn       
-    )
-
-    products_sold = (
-        items.groupby("product_id")
-        .size()
-        .reset_index(name="total_sold")
-        .sort_values(by="total_sold", ascending=False)
-    )
-
-    products_sold.to_sql("gold_top_products", conn, if_exists="replace", index=False)
-    
-
+        if __name__ == "__main__":
+            transform_data_silver_to_gold
     
     
     
